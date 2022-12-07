77fe6f541fdeb17485c5a09f2bb2a0d1a6a44ec0
minoli-g <minoliniroshani@gmail.com>
2022-12-07 12:35:31 +0530

    Read opcodes incoming and call function

--- main.cpp
+++ main.cpp
@@ -1,16 +1,30 @@
 #include "input_reader.hpp"
+#include "opcodes.hpp"
+#include "operations/all_operations.hpp"
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 int main(int argc, char *argv[]) {
     std::cout<<"Emulation started"<<"\n";
 
     InputReader ir = InputReader(argv[1]);
 
-    // Testing - output decimal values of every byte
-    std::cout<<std::to_string(ir.nextByte())<<"\n";
-    std::cout<<std::to_string(ir.nextByte())<<"\n";
-    std::cout<<std::to_string(ir.nextByte())<<"\n";
-    std::cout<<std::to_string(ir.nextByte())<<"\n";
+    std::vector<uint8_t> incoming_opcodes;
+
+    while (ir.fileRemaining()){
+       //std::cout<<std::to_string(ir.nextByte())<<"\n";
+
+       incoming_opcodes.push_back(ir.nextByte());
+
+       // Test for opcodes which need extra bytes
+
+       // Detect which operation is being referred to  and execute it
+       if (opcodes::add_ops.count(incoming_opcodes[0])){
+
+            //Call the add function
+            add(&ir, incoming_opcodes);
+       }
+    }
 }
--- makefile
+++ makefile
@@ -1,11 +1,20 @@
 CC = g++
 CFLAGS = -std=c++17 -Wall -g
 
-main: main.o input_reader.o
-	$(CC) $(CFLAGS) -o main main.o input_reader.o
+main: main.o input_reader.o modrm.o add.o opcodes.o
+	$(CC) $(CFLAGS) -o main main.o input_reader.o modrm.o add.o opcodes.o
 
 main.o: main.cpp
 	$(CC) $(CFLAGS) -c main.cpp 
 
 input_reader.o: input_reader.hpp input_reader.cpp
 	$(CC) $(CFLAGS) -c input_reader.cpp
+
+modrm.o: modrm.hpp modrm.cpp
+	$(CC) $(CFLAGS) -c modrm.cpp
+
+opcodes.o: opcodes.hpp opcodes.cpp
+	$(CC) $(CFLAGS) -c opcodes.cpp
+
+add.o: operations/add.hpp operations/add.cpp
+	$(CC) $(CFLAGS) -c operations/add.cpp 
