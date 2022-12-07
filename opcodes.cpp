ba04cd82d89901a601d286b7830ba2b6f1a22a4d
minoli-g <minoliniroshani@gmail.com>
2022-12-07 12:31:45 +0530

    Hardcode the opcodes

---
+++ opcodes.cpp
@@ -0,0 +1,8 @@
+#include "opcodes.hpp"
+
+namespace opcodes{
+
+    extern const std::set<std::uint8_t> add_ops = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x83};
+    // TODO - Other operations
+
+}
---
+++ opcodes.hpp
@@ -0,0 +1,14 @@
+#ifndef OPCODES_H
+#define OPCODES_H
+
+#include <set>
+#include <cstdint>
+
+namespace opcodes{
+
+    extern const std::set<std::uint8_t> add_ops;
+    //TODO - Add other operations
+
+}
+
+#endif
