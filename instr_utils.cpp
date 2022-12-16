dfbee6c5f04cb96fe6888c35346be78e79fe4fdc
minoli-g <minoliniroshani@gmail.com>
2022-12-16 22:09:07 +0530

    Decoding SIB + helper functions

--- instr_utils.cpp
+++ instr_utils.cpp
@@ -1,5 +1,21 @@
 #include "instr_utils.hpp"
 
+std::string regs_32bit[] = {"EAX", "ECX", "EDX", "EBX", "ESP", "EBP", "ESI", "EDI" };
+
+uint8_t getDisp8(InputReader* ir){
+    return ir->nextByte();
+}
+
+uint16_t getDisp16(InputReader* ir){
+    uint16_t disp16 = 0;
+
+    // Concatenate the next 2 bytes
+    disp16 |= (ir->nextByte()) << 8;
+    disp16 |= ir->nextByte();
+
+    return disp16;
+}
+
 uint32_t getDisp32(InputReader* ir){
     uint32_t disp32 = 0;
 
@@ -10,7 +26,34 @@ uint32_t getDisp32(InputReader* ir){
     return disp32;
 }
 
-uint32_t getSIBMemLocation(uint8_t sib){
-    //TODO
-    return 0; 
+uint32_t getSIBMemLocation(uint8_t sib, RegisterBank* rb, uint8_t mod, InputReader* ir){
+
+    int scale = sib >> 6;
+    int index = (sib>>3)&7;
+    int base = sib&7;
+
+    uint32_t base_value = 0;
+    if (base != 4){
+        base_value = rb->get(regs_32bit[base]);
+    }
+    else {
+        switch (mod){
+            case 0: 
+                base_value = getDisp32(ir);
+                break;
+            case 1:
+                base_value = getDisp8(ir) + rb->get("EBP");
+                break;
+            case 2:
+                base_value = getDisp32(ir) + rb->get("EBP");
+                break;    
+        }
+    }
+
+    uint32_t scaled_value = 0;
+    if (index != 5){
+        scaled_value += rb->get(regs_32bit[index]) * (1<<scale);
+    }
+
+    return base_value + scaled_value; 
 }
--- instr_utils.hpp
+++ instr_utils.hpp
@@ -2,9 +2,13 @@
 #define INSTR_UTILS_H
 
 #include <cstdint>
+#include "register.hpp"
 #include "input_reader.hpp"
 
-uint32_t getSIBMemLocation(uint8_t sib);
+uint32_t getSIBMemLocation(uint8_t sib, RegisterBank* rb, uint8_t mod, InputReader* ir);
+
+uint8_t getDisp8(InputReader* ir);
+uint16_t getDisp16(InputReader* ir);
 uint32_t getDisp32(InputReader* ir);
 
 #endif
