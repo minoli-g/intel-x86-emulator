00e700b5cff4bcaa76611beef3407035ef47a5fc
minoli-g <minoliniroshani@gmail.com>
2022-12-16 18:55:35 +0530

    Get register from RM

--- modrm.cpp
+++ modrm.cpp
@@ -1,5 +1,11 @@
 #include "modrm.hpp"
 
+std::string regs_32bit[] = {"EAX", "ECX", "EDX", "EBX", "ESP", "EBP", "ESI", "EDI" };
+std::string regs_16bit[] = { "AX", "CX", "DX", "BX", "SP", "BP", "SI", "DI" };
+std::string regs_8bit[] = { "AL", "CL", "DL", "BL", "AH", "CH", "DH", "BH" };
+//std::string regs_mmx[] = { "MM0", "MM1", "MM2", "MM3", "MM4", "MM5", "MM6", "MM7" };
+//std::string regs_xmm[] = { "XMM0", "XMM1", "XMM2", "XMM3", "XMM4", "XMM5", "XMM6", "XMM7" };
+
 uint8_t getModRM(InputReader* ir){
     return ir->nextByte();
 }
@@ -14,4 +20,33 @@ uint8_t getReg(uint8_t modrm){
 
 uint8_t getRM(uint8_t modrm){
     return (uint8_t) (modrm&7);
+}
+
+bool isRMReg(uint8_t modrm){
+    return (getMod(modrm)==11);
+}
+
+std::string getRMReg(uint8_t modrm, enum RegisterTypes reg_type){
+    int index = getRM(modrm);
+
+    switch (reg_type){
+        case REG_8:
+            return regs_8bit[index];
+            break;
+
+        case REG_16:
+            return regs_16bit[index];
+            break;
+
+        case REG_32:
+            return regs_32bit[index];
+            break;
+
+        default:
+            std::cout << "Unsupported register type \n";
+    }
+}
+
+uint32_t getRMMemLocation(uint8_t modrm){
+    
 }
--- modrm.hpp
+++ modrm.hpp
@@ -3,9 +3,17 @@
 
 #include "input_reader.hpp"
 
+enum RegisterTypes{
+    REG_8, REG_16, REG_32, REG_MM, REG_XMM
+};
+
 uint8_t getModRM(InputReader* ir);
 uint8_t getMod(uint8_t modrm);
 uint8_t getReg(uint8_t modrm);
 uint8_t getRM(uint8_t modrm);
+
+bool isRMReg(uint8_t modrm);
+std::string getRMReg(uint8_t modrm);
+uint32_t getRMMemLocation(uint8_t modrm);
 
 #endif
