f9bdf4d37817fc733c6583ec5ff58cd0bfa05b8c
minoli-g <minoliniroshani@gmail.com>
2022-12-19 19:13:49 +0530

    Implement add00 with reg, mem access

--- operations/add.cpp
+++ operations/add.cpp
@@ -1,22 +1,32 @@
 #include "add.hpp"
 #include "../modrm.hpp"
 
-void add_00(InputReader* ir){
+void add_00(InputReader* ir, RegisterBank* rb, Memory* mem){
 
     // 00/r - Add r8 to r/m8
-
     uint8_t modrm = ir->nextByte();
-    uint8_t r8 = getReg(modrm);
-    uint8_t rm8 = getRM(modrm);
+    std::cout<< +getMod(modrm) << +getRM(modrm) <<"\n";
 
-    std::cout<< "Adding value of Register "<< std::to_string(r8) 
-    << " to value of Register " << std::to_string(rm8) << "\n";
+    std::string r8 = getRegFromIndex(getReg(modrm), REG_8);
+
+    if (isRMReg(modrm)){
+        std::string rm8 = getRMReg(modrm, REG_8);
+        uint8_t result = rb->get(r8)+ rb->get(rm8);
+        rb->set(r8, result);
+    }
+    else{
+        uint32_t rm_mem = getRMMemLocation(modrm, rb, ir);
+        uint32_t result = rb->get(r8) + mem->read(rm_mem);
+        mem->write(rm_mem, result);
+    }
+
+    // Set flags - TODO
 }
 
-void add(InputReader* ir, uint8_t opcode){
+void add(InputReader* ir, RegisterBank* rb, Memory* mem, uint8_t opcode){
     switch (opcode) {
         case 0x00:
-            add_00(ir);
+            add_00(ir, rb, mem);
             break;
 
         default:
--- operations/add.hpp
+++ operations/add.hpp
@@ -2,16 +2,17 @@
 #define ADD_H
 
 #include "../input_reader.hpp"
-#include <vector>
+#include "../register.hpp"
+#include "../memory.hpp"
 
-void add_00(InputReader* ir);
-void add_01(InputReader* ir);
-void add_02(InputReader* ir);
-void add_03(InputReader* ir);
-void add_04(InputReader* ir);
-void add_05(InputReader* ir);
-void add_83(InputReader* ir);
+void add_00(InputReader* ir, RegisterBank* rb, Memory* mem);
+void add_01(InputReader* ir, RegisterBank* rb, Memory* mem);
+void add_02(InputReader* ir, RegisterBank* rb, Memory* mem);
+void add_03(InputReader* ir, RegisterBank* rb, Memory* mem);
+void add_04(InputReader* ir, RegisterBank* rb, Memory* mem);
+void add_05(InputReader* ir, RegisterBank* rb, Memory* mem);
+void add_83(InputReader* ir, RegisterBank* rb, Memory* mem);
 
-void add(InputReader* ir, uint8_t opcode);
+void add(InputReader* ir, RegisterBank* rb, Memory* mem, uint8_t opcode);
 
 #endif
