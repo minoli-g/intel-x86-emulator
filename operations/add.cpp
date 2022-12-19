38ec59530c14e88f047f90f02a213b353e8decbb
minoli-g <minoliniroshani@gmail.com>
2022-12-19 21:22:24 +0530

    Complete add operations

--- operations/add.cpp
+++ operations/add.cpp
@@ -1,11 +1,53 @@
 #include "add.hpp"
 #include "../modrm.hpp"
+#include "../instr_utils.hpp"
 
 void add_00(InputReader* ir, RegisterBank* rb, Memory* mem){
 
     // 00/r - Add r8 to r/m8
     uint8_t modrm = ir->nextByte();
-    std::cout<< +getMod(modrm) << +getRM(modrm) <<"\n";
+
+    std::string r8 = getRegFromIndex(getReg(modrm), REG_8);
+
+    if (isRMReg(modrm)){
+        std::string rm8 = getRMReg(modrm, REG_8);
+        uint8_t result = rb->get(r8)+ rb->get(rm8);
+        rb->set(rm8, result);
+    }
+    else{
+        uint32_t rm_mem = getRMMemLocation(modrm, rb, ir);
+        uint32_t result = rb->get(r8) + mem->read(rm_mem);
+        mem->write(rm_mem, result);
+    }
+
+    // Set flags - TODO
+}
+
+void add_01(InputReader* ir, RegisterBank* rb, Memory* mem){
+
+    // Add r32 to r/m32
+    uint8_t modrm = ir->nextByte();
+
+    std::string r32 = getRegFromIndex(getReg(modrm), REG_32);
+
+    if (isRMReg(modrm)){
+        std::string rm32 = getRMReg(modrm, REG_32);
+        uint32_t result = rb->get(r32)+ rb->get(rm32);
+        rb->set(rm32, result);
+    }
+    else{
+        uint32_t rm_mem = getRMMemLocation(modrm, rb, ir);
+        uint32_t result = rb->get(r32) + mem->read(rm_mem);
+        mem->write(rm_mem, result);
+    }
+
+    // Set flags - TODO
+}
+
+void add_02(InputReader* ir, RegisterBank* rb, Memory* mem){
+
+    // Add r/m8 to r8
+    uint8_t modrm = ir->nextByte();
 
     std::string r8 = getRegFromIndex(getReg(modrm), REG_8);
 
@@ -16,7 +58,37 @@ void add_00(InputReader* ir, RegisterBank* rb, Memory* mem){
     }
     else{
         uint32_t rm_mem = getRMMemLocation(modrm, rb, ir);
-        uint32_t result = rb->get(r8) + mem->read(rm_mem);
+        uint8_t result = (uint8_t) (rb->get(r8) + mem->read(rm_mem));
+        rb->set(r8, result);
+    }
+
+    // Set flags - TODO
+}
+
+void add_04(InputReader* ir, RegisterBank* rb, Memory* mem){
+
+    // Add imm8 to AL.
+    uint8_t imm8 = getImm8(ir);
+    uint32_t al = rb->get("AL");
+    rb->set("AL", al+imm8);
+
+    // Set flags - TODO
+}
+
+void add_83(InputReader* ir, RegisterBank* rb, Memory* mem){
+
+    // Add imm8 to r/m32.
+    uint8_t modrm = ir->nextByte();
+    uint8_t imm8 = getImm8(ir);
+
+    if (isRMReg(modrm)){
+        std::string rm32 = getRMReg(modrm, REG_32);
+        uint32_t result = imm8 + rb->get(rm32);
+        rb->set(rm32, result);
+    }
+    else{
+        uint32_t rm_mem = getRMMemLocation(modrm, rb, ir);
+        uint32_t result = imm8 + mem->read(rm_mem);
         mem->write(rm_mem, result);
     }
 
@@ -29,7 +101,23 @@ void add(InputReader* ir, RegisterBank* rb, Memory* mem, uint8_t opcode){
             add_00(ir, rb, mem);
             break;
 
+        case 0x01:
+            add_01(ir, rb, mem);
+            break;
+
+        case 0x02:
+            add_02(ir, rb, mem);
+            break;
+
+        case 0x04:
+            add_04(ir, rb, mem);
+            break;
+
+        case 0x83:
+            add_83(ir, rb, mem);
+            break;
+
         default:
-            std::cout<<"Add error \n";
+            std::cout<<"Unsupported addition opcode \n";
     }
 }
--- operations/add.hpp
+++ operations/add.hpp
@@ -8,9 +8,7 @@
 void add_00(InputReader* ir, RegisterBank* rb, Memory* mem);
 void add_01(InputReader* ir, RegisterBank* rb, Memory* mem);
 void add_02(InputReader* ir, RegisterBank* rb, Memory* mem);
-void add_03(InputReader* ir, RegisterBank* rb, Memory* mem);
 void add_04(InputReader* ir, RegisterBank* rb, Memory* mem);
-void add_05(InputReader* ir, RegisterBank* rb, Memory* mem);
 void add_83(InputReader* ir, RegisterBank* rb, Memory* mem);
 
 void add(InputReader* ir, RegisterBank* rb, Memory* mem, uint8_t opcode);
